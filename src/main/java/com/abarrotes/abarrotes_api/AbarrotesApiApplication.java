439435a6573fcdec74f2cd3ecc4f683825091ce1
EleazarDev <eleazarjarquincv@gmail.com>
2025-09-23 14:08:53 -0600

    start: initial commit

---
+++ src/main/java/com/abarrotes/abarrotes_api/AbarrotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.abarrotes.abarrotes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbarrotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbarrotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abarrotes/abarrotes_api/AbarrotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abarrotes.abarrotes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbarrotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
