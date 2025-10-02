83ee115b6db9d51393fb90df4ccdfeda89b72b01
EleazarDev <eleazarjarquincv@gmail.com>
2025-10-02 12:12:51 -0600

    Start: Initial commit

---
+++ src/main/java/com/unsis/scunsis_backend/ScunsisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unsis.scunsis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScunsisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScunsisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unsis/scunsis_backend/ScunsisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsis.scunsis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScunsisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
