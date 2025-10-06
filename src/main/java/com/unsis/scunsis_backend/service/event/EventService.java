774f06843952897c03d02a9963fec3d115765231
EleazarDev <eleazarjarquincv@gmail.com>
2025-10-06 12:50:53 -0600

    feat: Se agregan controllers para eventos

--- src/main/java/com/unsis/scunsis_backend/service/event/EventService.java
+++ src/main/java/com/unsis/scunsis_backend/service/event/EventService.java
@@ -1,6 +1,15 @@
 package com.unsis.scunsis_backend.service.event;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import com.unsis.scunsis_backend.dto.request.event.EventRequest;
+import com.unsis.scunsis_backend.dto.response.event.EventResponse;
+import com.unsis.scunsis_backend.mapper.event.EventMapper;
+import com.unsis.scunsis_backend.model.event.Event;
+import com.unsis.scunsis_backend.repository.event.IEventRepository;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -8,4 +17,27 @@ import lombok.RequiredArgsConstructor;
 @Data
 @Service
 @RequiredArgsConstructor
-public class EventService {}
+public class EventService {
+
+    private final EventMapper eventMapper;
+    private final IEventRepository eventRepository;
+
+    public List<EventResponse> getAllEvent(){
+        List<EventResponse> events = eventMapper.toDtos(eventRepository.findAll());
+        return events;
+    }
+
+    public void createEvent(EventRequest request){
+        eventRepository.save(eventMapper.toEntity(request));
+    }
+
+    public EventResponse getEventById(Long eventId){
+        Optional<Event> event = eventRepository.findById(eventId);
+            EventResponse eventResponse = eventMapper.toDto(event.get());
+            return eventResponse;
+    }
+
+    public void deleteEventById(long eventId){
+        eventRepository.deleteById(eventId);
+    }
+}
