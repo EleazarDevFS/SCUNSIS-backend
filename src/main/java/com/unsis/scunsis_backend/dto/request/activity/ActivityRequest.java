72fcb75a035b759e21dd969c9e2870df4b2f02b9
EleazarDev <eleazarjarquincv@gmail.com>
2025-10-06 16:56:20 -0600

    feat: Se agrega mapper & dto's activity

--- src/main/java/com/unsis/scunsis_backend/dto/request/activity/ActivityRequest.java
+++ src/main/java/com/unsis/scunsis_backend/dto/request/activity/ActivityRequest.java
@@ -1,5 +1,34 @@
 package com.unsis.scunsis_backend.dto.request.activity;
 
+import java.sql.Date;
+
+import com.unsis.scunsis_backend.model.event.Event;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivityRequest {
+
+    private long activityId;
+
+    private Event eventId;
+
+    private String activityName;
+
+    private String activityDescription;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private String activityPlace;
 
 }
--- src/main/java/com/unsis/scunsis_backend/mapper/activity/ActivityMapper.java
+++ src/main/java/com/unsis/scunsis_backend/mapper/activity/ActivityMapper.java
@@ -1,8 +1,63 @@
 package com.unsis.scunsis_backend.mapper.activity;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.unsis.scunsis_backend.dto.request.activity.ActivityRequest;
+import com.unsis.scunsis_backend.dto.response.activity.ActivityResponse;
+import com.unsis.scunsis_backend.mapper.BaseMapper;
+import com.unsis.scunsis_backend.model.activity.Activity;
+
 import lombok.Data;
 
 @Data
-public class ActivityMapper {
+public class ActivityMapper implements BaseMapper<ActivityResponse, ActivityRequest, Activity> {
+    
+    @Override
+    public Activity toEntity(ActivityRequest dto) {
+        return Activity.builder()
+        .activityDescription(dto.getActivityDescription())
+        .activityName(dto.getActivityName())
+        .activityPlace(dto.getActivityPlace())
+        .endDate(dto.getEndDate())
+        .eventId(dto.getEventId())
+        .startDate(dto.getStartDate())
+        .build();
+    }
+
+    public Activity toEntity(ActivityResponse dto) {
+        return Activity.builder()
+        .activityDescription(dto.getActivityDescription())
+        .activityName(dto.getActivityName())
+        .activityPlace(dto.getActivityPlace())
+        .endDate(dto.getEndDate())
+        .eventId(dto.getEventId())
+        .startDate(dto.getStartDate())
+        .build();
+    } 
+
+    @Override
+    public ActivityResponse toDto(Activity entity) {
+       return ActivityResponse.builder()
+       .activityDescription(entity.getActivityDescription())
+       .activityName(entity.getActivityName())
+       .activityPlace(entity.getActivityPlace())
+       .endDate(entity.getEndDate())
+       .eventId(entity.getEventId())
+       .startDate(entity.getStartDate())
+       .build();
+    }
+
+    @Override
+    public List<ActivityResponse> toDtos(List<Activity> entities) {
+       return entities.stream()
+       .map(this::toDto)
+       .collect(Collectors.toList());
+    }
+
+    @Override
+    public void updateEntity(ActivityRequest request, Activity entity) {
+        throw new UnsupportedOperationException("Unimplemented method 'updateEntity'");
+    }
 
 }
