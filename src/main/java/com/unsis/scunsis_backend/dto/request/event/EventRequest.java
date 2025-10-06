f3d70c3c5848ae8818938e896090f89c10dec271
EleazarDev <eleazarjarquincv@gmail.com>
2025-10-06 08:41:37 -0600

    refact: Se agrega constructor BUilder

--- src/main/java/com/unsis/scunsis_backend/dto/request/event/EventRequest.java
+++ src/main/java/com/unsis/scunsis_backend/dto/request/event/EventRequest.java
@@ -1,5 +1,29 @@
 package com.unsis.scunsis_backend.dto.request.event;
 
+import java.sql.Date;
+
+import com.unsis.scunsis_backend.model.enums.EEventType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class EventRequest {
 
+    private EEventType eventType;
+
+    private String eventName;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private String eventPlace;
+
+    private String eventDescription;
 }
--- src/main/java/com/unsis/scunsis_backend/dto/response/event/EventResponse.java
+++ src/main/java/com/unsis/scunsis_backend/dto/response/event/EventResponse.java
@@ -1,5 +1,28 @@
 package com.unsis.scunsis_backend.dto.response.event;
 
-public class EventResponse {
+import java.sql.Date;
 
+import com.unsis.scunsis_backend.model.enums.EEventType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventResponse {
+    private EEventType eventType;
+
+    private String eventName;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private String eventPlace;
+
+    private String eventDescription;
 }
