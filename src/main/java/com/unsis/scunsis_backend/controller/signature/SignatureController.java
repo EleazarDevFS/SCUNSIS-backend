e7dd40fda6d7c6f61bde4b3f02f1b9979199f7b4
EleazarDev <eleazarjarquincv@gmail.com>
2025-10-09 16:42:16 -0600

    feat: se agrega signature crud

--- src/main/java/com/unsis/scunsis_backend/controller/signature/SignatureController.java
+++ src/main/java/com/unsis/scunsis_backend/controller/signature/SignatureController.java
@@ -1,10 +1,48 @@
 package com.unsis.scunsis_backend.controller.signature;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unsis.scunsis_backend.dto.request.signature.SignatureRequest;
+import com.unsis.scunsis_backend.dto.response.signature.SignatureResponse;
+import com.unsis.scunsis_backend.service.signature.SignatureService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/scunsis/api/v1/signature")
+@RequiredArgsConstructor
 public class SignatureController {
 
+    private final SignatureService signatureService;
+
+    @GetMapping("/{signatureId}")
+    public ResponseEntity<SignatureResponse> getById(@PathVariable long signatureId){
+        return ResponseEntity.ok(signatureService.getById(signatureId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SignatureResponse>> getAll(){
+        return ResponseEntity.ok(signatureService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createSignature(@RequestBody SignatureRequest request){
+        signatureService.createSignature(request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{signatureId}")
+    public ResponseEntity<Void> deleteById(@PathVariable long signatureId){
+        signatureService.deleteById(signatureId);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/unsis/scunsis_backend/mapper/signature/SignatureMapper.java
+++ src/main/java/com/unsis/scunsis_backend/mapper/signature/SignatureMapper.java
@@ -1,8 +1,47 @@
 package com.unsis.scunsis_backend.mapper.signature;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.unsis.scunsis_backend.dto.request.signature.SignatureRequest;
+import com.unsis.scunsis_backend.dto.response.signature.SignatureResponse;
+import com.unsis.scunsis_backend.mapper.BaseMapper;
+import com.unsis.scunsis_backend.model.signature.Signature;
+
 import lombok.Data;
 
 @Data
-public class SignatureMapper {
+@Component
+public class SignatureMapper implements BaseMapper<SignatureResponse, SignatureRequest, Signature>{
+    
+    @Override
+    public Signature toEntity(SignatureRequest dto) {
+        return Signature.builder()
+        .academicGrade(dto.getAcademicGrade())
+        .lastName(dto.getLastName())
+        .name(dto.getName())
+        .position(dto.getPosition())
+        .twoLastName(dto.getTwoLastName())
+        .build();
+    }
+
+    @Override
+    public SignatureResponse toDto(Signature entity) {
+        return SignatureResponse.builder().build();
+    }
+
+    @Override
+    public List<SignatureResponse> toDtos(List<Signature> entities) {
+        return entities.stream()
+        .map(this::toDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public void updateEntity(SignatureRequest request, Signature entity) {
+        throw new UnsupportedOperationException("Unimplemented method 'updateEntity'");
+    }
+
 
 }
