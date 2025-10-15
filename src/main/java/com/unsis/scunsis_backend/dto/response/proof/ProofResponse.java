5a2602d018ff93383f8e201df15d70864ee6669f
EleazarDev <eleazarjarquincv@gmail.com>
2025-10-14 20:28:10 -0600

    feat: Se agrega Crud Signature

--- src/main/java/com/unsis/scunsis_backend/dto/response/proof/ProofResponse.java
+++ src/main/java/com/unsis/scunsis_backend/dto/response/proof/ProofResponse.java
@@ -1,5 +1,34 @@
 package com.unsis.scunsis_backend.dto.response.proof;
 
+import java.sql.Date;
+
+import com.unsis.scunsis_backend.model.activity.Activity;
+import com.unsis.scunsis_backend.model.enums.EProofType;
+import com.unsis.scunsis_backend.model.event.Event;
+import com.unsis.scunsis_backend.model.receiver.Receiver;
+import com.unsis.scunsis_backend.model.sender.Sender;
+import com.unsis.scunsis_backend.model.signature.Signature;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ProofResponse {
 
+    private String folio;
+
+    private Signature signature;
+
+    private Sender sender;
+
+    private Receiver receiver;    
+
+    private Activity activity;
+
+    private Event event;
+
+    private EProofType proofType;
+
+    private Date date;
 }
