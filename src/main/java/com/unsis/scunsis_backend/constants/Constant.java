2204f82047baa748f9cb1cee8911d36a5fde1f06
EleazarDev <eleazarjarquincv@gmail.com>
2026-07-06 23:34:47 -0600

    refact: coherence in constant name

--- src/main/java/com/unsis/scunsis_backend/constants/Constant.java
+++ src/main/java/com/unsis/scunsis_backend/constants/Constant.java
@@ -1,5 +1,5 @@
 package com.unsis.scunsis_backend.constants;
 
 public class Constant {
-    public static final String RECEIVER_NOT_FOUND = "No existe registro en la base de datos con id: %d";
+    public static final String NOT_FOUND_BY_ID = "No existe registro en la base de datos con id: %d";
 }
