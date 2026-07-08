aedb6978985a46dc511d1d0ede599063a0b0f8d7
EleazarDev <eleazarjarquincv@gmail.com>
2026-07-08 10:35:58 -0600

    fix: Resolve register the same sender with a different campus

--- src/main/java/com/unsis/scunsis_backend/repository/sender/ISenderRepository.java
+++ src/main/java/com/unsis/scunsis_backend/repository/sender/ISenderRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.unsis.scunsis_backend.model.sender.Sender;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ISenderRepository extends JpaRepository<Sender, Long> {
     Optional<Sender> findById(Long senderId);
-    Boolean existsByName(String senderName);
+    List<Sender> getSenderByName(String senderName);
 }
