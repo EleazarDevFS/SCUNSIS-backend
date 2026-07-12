017d31133184ceccae035e356ffbc5828948b750
EleazarDev <eleazarjarquincv@gmail.com>
2026-07-12 10:01:13 -0600

    refactor(repo): add queries for role counting and email lookup

--- src/main/java/com/unsis/scunsis_backend/repository/receiver/IReceiverRepository.java
+++ src/main/java/com/unsis/scunsis_backend/repository/receiver/IReceiverRepository.java
@@ -1,10 +1,13 @@
 package com.unsis.scunsis_backend.repository.receiver;
 
+import com.unsis.scunsis_backend.model.receiver.Receiver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.unsis.scunsis_backend.model.receiver.Receiver;
+import java.util.Optional;
+
 @Repository
-public interface IReceiverRepository extends JpaRepository<Receiver, Long>{
-    Boolean existsByEmail(String email);
+public interface IReceiverRepository extends JpaRepository<Receiver, Long> {
+    boolean existsByEmail(String email);
+    Optional<Receiver> findByEmail(String email);
 }
