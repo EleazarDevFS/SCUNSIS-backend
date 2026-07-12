aae803a4103efae92aaa13da393bb8d38c4d1b32
EleazarDev <eleazarjarquincv@gmail.com>
2026-07-12 10:09:21 -0600

    refactor(controller): fix endpoints, add new routes

--- src/main/java/com/unsis/scunsis_backend/controller/event/EventController.java
+++ src/main/java/com/unsis/scunsis_backend/controller/event/EventController.java
@@ -1,49 +1,41 @@
 package com.unsis.scunsis_backend.controller.event;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-
 import com.unsis.scunsis_backend.dto.request.event.EventRequest;
 import com.unsis.scunsis_backend.dto.response.event.EventResponse;
 import com.unsis.scunsis_backend.service.event.EventService;
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/scunsis/api/v1/event")
+@RequiredArgsConstructor
 public class EventController {
 
     private final EventService eventService;
-    
+
     @PostMapping
-    public void createEvent(@RequestBody EventRequest request){
+    public ResponseEntity<Void> createEvent(@RequestBody EventRequest request) {
         eventService.createEvent(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/{eventId}")
-    public ResponseEntity<Void> deleteEventById(@PathVariable Long eventId){
+    public ResponseEntity<Void> deleteEventById(@PathVariable Long eventId) {
         eventService.deleteEventById(eventId);
         return ResponseEntity.noContent().build();
     }
-    
+
     @GetMapping("/{eventId}")
-    public ResponseEntity<EventResponse> getEventById(@PathVariable Long eventId){
+    public ResponseEntity<EventResponse> getEventById(@PathVariable Long eventId) {
         return ResponseEntity.ok(eventService.getEventById(eventId));
     }
 
     @GetMapping
-    public ResponseEntity<List<EventResponse>> getAllEvent(){
+    public ResponseEntity<List<EventResponse>> getAllEvent() {
         return ResponseEntity.ok(eventService.getAllEvent());
     }
-
 }
